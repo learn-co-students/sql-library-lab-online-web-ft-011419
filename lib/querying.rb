4b42b2c23ec4cd36be9950e252e9cfa6fb9c5af9
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-14 21:00:44 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM Books  WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT Characters.name , Characters.motto FROM Characters  ORDER BY motto DESC LIMIT 1;"
 end
 
 
